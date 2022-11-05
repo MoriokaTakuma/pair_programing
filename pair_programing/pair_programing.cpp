79bf7390af19ab6340b161e875b548a37fb54c4e
Morioka <morioka0607space@gmail.com>
2022-11-05 12:39:30 +0900

    追加

--- pair_programing/pair_programing.cpp
+++ pair_programing/pair_programing.cpp
@@ -69,7 +69,34 @@ void draw_meiro(void)
     printf("move:←↑→↓&restart:ESC\n"); //操作説明
 
 }
+/* キー入力判定 */
+void key_input(void)
+{
+    int key;
+    while (1) {	/* キーが押されるまで待つ */
+        if (kbhit()) {
+            key = getch();	/* 入力されたキー番号 */
+            break;
+        }
+    }
+    if (key == 72 && meiro[py - 1][px] == 0)			/* ↑キー */
+        py--;	/* 上に移動 */
 
+    else if (key == 80 && meiro[py + 1][px] == 0)		/* ↓キー */
+        py++;	/* 下に移動 */
+
+    else if (key == 75 && meiro[py][px - 1] == 0)		/* ←キー */
+        px--;	/* 左に移動 */
+
+    else if (key == 77 && meiro[py][px + 1] == 0)		/* →キー */
+        px++;	/* 右に移動 */
+
+    else if (key == 27)								/* ESCキー */
+        play_start();	/* 最初の状態に戻る */
+
+    else											/* 上記以外のキーの場合は */
+        key_input();								/* 再度キー入力受付 */
+}
 
 int main()
 {
